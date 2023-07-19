2d32074d1be44c278bf89c5ef03a2d4bf8cfc6b3
Sergio05-dot <sergiohcarvalho05@gmail.com>
2023-07-19 16:59:30 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contacto', function () {
+    return "Estas en el formulario de contacto";
+});
